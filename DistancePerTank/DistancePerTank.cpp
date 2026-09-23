a6925b5c2024f5850278e28b1d9c5507d7e08124
Javier Toscano <jst2476@email.vccs.edu>
2026-09-23 17:46:15 -0400

    created project and added my doubles

--- DistancePerTank/DistancePerTank.cpp
+++ DistancePerTank/DistancePerTank.cpp
@@ -3,5 +3,13 @@
 using namespace std;
 int main()
 {
+	cout << "Total distance car can travel!" << endl;
+	//Constants
+	const double tankCapacity = 20;
+	const double townMPG = 23.5;
+	const double highwayMPG = 28.9;
+	double distanceTown = tankCapacity * townMPG;
+	double distanceHighway = tankCapacity * highwayMPG;
+
 
 }
