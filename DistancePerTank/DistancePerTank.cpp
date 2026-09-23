8bcda6ae09c71fe0363e375de4f8d4937fc1e84a
Javier Toscano <jst2476@email.vccs.edu>
2026-09-23 17:58:33 -0400

    still not understanding commits, I thought I had saved and pushed 3 and only the last step sent. restarting.

--- DistancePerTank/DistancePerTank.cpp
+++ DistancePerTank/DistancePerTank.cpp
@@ -3,17 +3,6 @@
 using namespace std;
 int main()
 {
-	cout << "Total distance car can travel!" << endl;
-	//Constants
-	const double tankCapacity = 20;
-	const double townMPG = 23.5;
-	const double highwayMPG = 28.9;
-	double distanceTown = tankCapacity * townMPG;
-	double distanceHighway = tankCapacity * highwayMPG;
-	// Displays
-	cout << "Full tank of gas in town: " << distanceTown;
-	cout << " miles" << endl;
-	cout << "Full tank of gas on the highway: " << distanceHighway; cout << " miles" << endl;
-	return 0;
+
 
 }
